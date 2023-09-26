d299459f1e421df633c6484651443803d6cc0bde
Efnalim <135123396+Efnalim@users.noreply.github.com>
2023-09-26 22:31:41 +0200

    added routers

--- router/main.py
+++ router/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI, APIRouter
 import uvicorn
 
+from routers import login, worship, hymn
+
 app = FastAPI()
-class HelloWorld():
-    def read_hello(self):
-        return {"data": "Hello World"}
-router = APIRouter()
-router.add_api_route('/api/v2/hello-world', 
-endpoint = HelloWorld().read_hello, methods=["GET"])
-app.include_router(router)
+
+app.include_router(login.router)
+app.include_router(hymn.router)
+app.include_router(worship.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Gateway of the App"}
 
 if __name__ == "__main__":
    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
